83f3c33bafe2c92f8903e3fc0a72ad1e94fc84b4
✪ vi4m Marcin Kliks <vi4m@users.noreply.github.com>
2017-01-06 17:02:56 +0100

    Update Shell.swift

--- Sources/Shell.swift
+++ Sources/Shell.swift
@@ -16,7 +16,7 @@ enum CommandError: Error {
 
 
 final class Shell {
-
+    
     static func systemWithTTY(cmd: String, args: [String]) throws {
         return try Terminal(arguments: []).execute(program: cmd, arguments: args)
     }
@@ -30,12 +30,10 @@ final class Shell {
         task.launchPath = cmd
         task.arguments = args
         let outHandle = outputPipe.fileHandleForReading
-
         let errorHandle = errorPipe.fileHandleForReading
 
         outHandle.readabilityHandler = { pipe in
             if let line = String(data: pipe.availableData, encoding: String.Encoding.utf8) {
-                // Update your view with the new text here
                 if !silent {
                     print(line, terminator: "")
                 }
@@ -62,10 +60,8 @@ final class Shell {
         outHandle.readabilityHandler = nil
 
         let status = task.terminationStatus
-
         if status != 0 {
             throw CommandError.NonZeroReturnCode
         }
     }
-
 }
